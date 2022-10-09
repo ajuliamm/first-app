37845b0c88b2d89b02a75bf37cd3ec8b8082780d
Angelina Julia <ajuliammelo@gmail.com>
2022-10-08 21:06:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Image, TextContainer, H1, Label, Input, Button } from './styles';
 
-function App() {
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+
+const App = () =>{
+  let hello = 'Olá'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Container>
+    <Image alt='people-image' src={People}/>
+    <TextContainer>
+     <H1>{hello}</H1>
+
+     <Label>Nome</Label>
+     <Input placeholder='Nome'/>
+
+     <Label>Idade</Label>
+     <Input placeholder='25'/>
+
+     <Button>Cadastrar<img alt='arrow' src={Arrow}/></Button>
+    </TextContainer>
+  </Container>
+  )
 }
 
 export default App;
